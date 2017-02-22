05279d6940c4792eefdffd9d53ad0b45fa75f513
Bill Zito <bill.r.zito@gmail.com>
2017-02-22 14:44:17 -0800

    Use yuv and ycrcb colorspaces

--- explore.py
+++ explore.py
@@ -33,19 +33,25 @@ def plot3d(pixels, colors_rgb,
 
 if __name__ == '__main__':
     # Read a color image
-    img = cv2.imread("/Users/billzito/Downloads/000275.png")
+    img = cv2.imread("/Users/billzito/Downloads/53.png")
     # Select a small fraction of pixels to plot by subsampling it
     scale = max(img.shape[0], img.shape[1], 64) / 64  # at most 64 rows and columns
+    # print('scale', scale)
     img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)), interpolation=cv2.INTER_NEAREST)
 
     # Convert subsampled image to desired color space(s)
-    img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
+    img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes YUV
+    img_small_YUV = cv2.cvtColor(img_small, cv2.COLOR_BGR2YUV)  # OpenCV uses BGR, matplotlib likes YUV
+    img_small_YCrCb = cv2.cvtColor(img_small, cv2.COLOR_BGR2YCrCb)
     img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
     img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
 
     # Plot and show
-    plot3d(img_small_RGB, img_small_rgb)
+    plot3d(img_small_YUV, img_small_rgb, axis_labels=list("YUV"))
     plt.show()
 
-    plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
-    plt.show()
+    # plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+    # plt.show()
+
+    # plot3d(img_small_YCrCb, img_small_rgb, axis_labels=list("Yrb"))
+    # plt.show()
